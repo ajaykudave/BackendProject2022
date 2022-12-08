e61c7aff703301a9c305a83508377e6436a51ad9
Ajay kudave <jaykudave@gmail.com>
2022-12-08 11:31:13 +0530

    Added Routes for Auth.js file in controller

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router  = express.Router();
 
 console.log('Inside Route file');
-const { registerUser , login , getMe} = require('../controllers/auth.js'); //destructuring
+const { registerUser , login , getMe , forgetPassword , resetPassword ,updateUserDetails ,updatePassword} = require('../controllers/auth.js'); //destructuring
 
-const { protect ,authorize} = require('../middlewares/auth.js');
+const { protect } = require('../middlewares/auth.js');
 router
 .route('/register')
 .post(registerUser); 
@@ -16,7 +16,24 @@ router
 //this route is for bringing logined User data..here we put protect becasue where we check the token we got at the time of login and also we fetch login user data and attached to req objcet..so that it used in controller 
 router
 .route('/me')
-.get(protect , authorize('publisher' , 'admin') , getMe);
+.get(protect , getMe);
+// .get(protect , authorize('publisher' , 'admin') , getMe);
+
+router
+.route('/forgetPassword')
+.post(forgetPassword);
+
+router
+.route('/resetPassword/:resetToken')
+.put(resetPassword);
+
+router
+.route('/updateUserDetails')
+.put(protect , updateUserDetails);
+
+router
+.route('/updatePassword')
+.put(protect ,updatePassword);
 /* router
 .post('/register' , registerUser)...we can write route this way also */
 
